a7c52303bbc70c69c0e0385ce347ebe97a90bea2
amelieykw <yu.kaiwen.ykw@outlook.com>
2019-09-17 16:43:43 +0800

    modify day 01's workshop.js

--- week-01/day-01/workshop.js
+++ week-01/day-01/workshop.js
@@ -271,7 +271,54 @@ console.log();
 
 
 // ex 11 - variable-mutation
+let a = 3;
+// make it bigger by 10
 
+console.log(a);
+
+let b = 100;
+// make it smaller by 7
+
+console.log(b);
+
+let c = 44;
+// double c's value
+
+console.log(c);
+
+let d = 125;
+// divide d's value by 5
+
+console.log(d);
+
+let e = 8;
+// what's the cube of e's value?
+
+console.log(e);
+
+const f1 = 123;
+const f2 = 345;
+// tell if f1 is bigger than f2 (as a boolean)
+
+const g1 = 350;
+const g2 = 200;
+// tell if the double of g2 is bigger than g1 (as a boolean)
+
+
+const h = 1357988018575474;
+// tell if h has 11 as a divisor (as a boolean)
+
+const i1 = 10;
+const i2 = 3;
+// tell if i1 is higher than i2 squared and smaller than i2 cubed (as a boolean)
+
+const j = 1521;
+// tell if j is dividable by 3 or 5 (as a boolean)
+
+let k = 'Apple';
+// fill the k variable with its content 4 times
+
+console.log(k);
 
 // ex 12 - cuboid
 // Write a program that stores 3 sides of a cuboid as variables (floats)
@@ -285,16 +332,15 @@ console.log();
 const currentHours = 14;
 const currentMinutes = 34;
 const currentSeconds = 42;
-
 // Write a program that prints the remaining seconds (as an integer) from a
 // day if the current time is represented by these variables
 
 
 
 // Nothings
-undefined;
-null;
-NaN;
+// undefined;
+// null;
+// NaN;
 
 null !== undefined;
 NaN !== undefined;
