54d51b40e16623f3b81a4a84a1735021c1bce248
amelieykw <yu.kaiwen.ykw@outlook.com>
2019-09-18 14:52:22 +0800

    week 01 day 03 products database 2 finish

--- week-01/day-03/workshop.js
+++ week-01/day-03/workshop.js
@@ -279,3 +279,38 @@ function cheapestProduct (products) {
 }
 console.log(cheapestProduct(products));
 console.log();
+
+console.log("================= Data Structures : ex 05 ==================")
+// Product database 2
+// We are going to represent our products in a map where the keys are strings representing the product's name and the values are numbers representing the product's price.
+
+// Create a map with the following key-value pairs.
+
+// Product name (key)	Price (value)
+// Eggs	200
+// Milk	200
+// Fish	400
+// Apples	150
+// Bread	50
+// Chicken	550
+let products2 = {
+    'Eggs': 200,
+    'Milk': 200,
+    'Fish': 400,
+    'Apples': 150,
+    'Bread': 50,
+    'Chicken': 550
+}
+// Create an application which solves the following problems.
+
+// Which products cost less than 201? (just the name)
+function productsLessThan201 (products2) {
+    return Object.keys(products2).filter(key => products2[key] < 201);
+}
+console.log(productsLessThan201(products2));
+// Which products cost more than 150? (name + price)
+function productsMoreThan150 (products2) {
+    return Object.keys(products2).filter(key => products2[key] > 150);
+}
+console.log(productsMoreThan150(products2));
+console.log();
