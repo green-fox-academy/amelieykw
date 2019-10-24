efccee76093f2b21a730d6691d6201856ef93084
amelieykw <yu.kaiwen.ykw@outlook.com>
2019-10-24 14:59:19 +0800

    week 05 day 04 greetings successfully catch query from url by expressjs and render it to ui

---
+++ week-05-greetings/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// home page
+app.get('/', (req, res) => {
+    res.render('home', {
+        title: 'Welcome back',
+        username: req.query.name
+    });
+});
+
+// start express app on port 3000
+let server = app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
