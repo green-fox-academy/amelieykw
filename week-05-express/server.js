a0d24eae3a5751a276565240e169d4f1701dc8eb
amelieykw <yu.kaiwen.ykw@outlook.com>
2019-10-24 12:18:16 +0800

    week 05 day 04 render the index.html

--- week-05-express/server.js
+++ week-05-express/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res) {
-    res.send('Hello World!');
+    // res.send('Hello World!');
+    res.sendFile(__dirname + '/index.html');
 });
 let server = app.listen(3000, function() {});
 
