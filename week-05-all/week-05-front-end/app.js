81b6c0e9d34cd65e1b6480db6999fb617c729887
amelieykw <yu.kaiwen.ykw@outlook.com>
2019-10-25 14:44:01 +0800

    week 05 day 05 add Doubling part

--- week-05-all/week-05-front-end/app.js
+++ week-05-all/week-05-front-end/app.js
@@ -9,6 +9,20 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/doubling', (req, res) => {
+    let received = req.query.input;
+    if (received === undefined) {
+        res.send({
+            "error": "Please provide an input"
+        })
+    } else {
+        res.send({
+            "received": received,
+            "result": received * 2
+        });
+    }
+});
+
 // start express app on port 3000
 let server = app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
