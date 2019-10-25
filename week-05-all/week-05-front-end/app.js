9bdb63c03f9cd1d3f483152d7f1f4a47dece2cab
amelieykw <yu.kaiwen.ykw@outlook.com>
2019-10-25 15:00:17 +0800

    week 05 day 05 add greeter part

--- week-05-all/week-05-front-end/app.js
+++ week-05-all/week-05-front-end/app.js
@@ -23,6 +23,29 @@ app.get('/doubling', (req, res) => {
     }
 });
 
+app.get('/greeter', (req, res) => {
+    let name = req.query.name;
+    let title = req.query.title;
+
+    if (name === undefined && title === undefined) {
+        res.send({
+            "error": "Please provide a name and a title"
+        })
+    } else if (name === undefined) {
+        res.send({
+            "error": "Please provide a name"
+        })
+    } else if (title === undefined) {
+        res.send({
+            "error": "Please provide a title"
+        })
+    } else {
+        res.send({
+            "welcome_message": "Oh, hi there " + name + ", my dear " + title
+        });
+    }
+});
+
 // start express app on port 3000
 let server = app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
