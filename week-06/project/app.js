95ff672cda9488beb6969aeeafaaa519a03ca3b3
amelieykw <yu.kaiwen.ykw@outlook.com>
2019-11-04 15:50:09 +0800

    week 07 day 01 connet backend with local mysql database reddit and insert initial posts and comments into database

--- week-06/project/app.js
+++ week-06/project/app.js
@@ -2,8 +2,8 @@
 var express = require('express');
 const mysql = require('mysql');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
 const cors = require('cors');
 
 // var indexRouter = require('./routes/index');
@@ -48,7 +48,7 @@ const mysqldb = mysql.createConnection({
   user: 'root',
   password: 'root',
   port: 3308,
-  database: 'week7project'
+  database: 'reddit'
 });
 
 mysqldb.connect(function (err) {
@@ -57,21 +57,75 @@ mysqldb.connect(function (err) {
   }
   console.log('connection successully');
 
-  let createTableUsers = `CREATE TABLE IF NOT EXISTS users(
+  let createTablePosts = `CREATE TABLE IF NOT EXISTS posts(
       id int primary key auto_increment,
-      name varchar(255) not null
+      title TEXT not null,
+      url TEXT not null,
+      timestamp TIMESTAMP not null,
+      score int not null default 0,
+      owner TEXT default null,
+      vote int default 0
     )`;
 
-    mysqldb.query(createTableUsers, function (err, results, fields) {
-    if (err) {
-      console.log(err.message);
-    }
-  });
+  let createTableComments = `CREATE TABLE IF NOT EXISTS comments(
+        id int primary key auto_increment,
+        postId int not null,
+        text TEXT not null,
+        score int not null default 0,
+        owner TEXT default null
+      )`;
+
+  [createTablePosts, createTableComments].forEach(query => executeSQLquery(query));
+
+  let insertInitialPosts = `INSERT INTO posts (id, title, url, score, owner, vote) VALUES 
+    (25, "Dear JavaScript", "http://9gag.com", 791, null, 1),
+    (74, "Crockford", "http://9gag.com", 567, "kristof4", -1)
+  `;
+
+  let insertInitialComments = `INSERT INTO comments (id, postId, text, score, owner) VALUES 
+    (17, 25, "Yes it is!", 12, "someone"),
+    (22, 25, "No it's not!", 9, "someoneelse"),
+    (11, 25, "That's a good post", 8, "someone");
+    `;
+
+  let checkIfEmptyPosts = `SELECT * FROM posts`;
+  let checkIfEmptyComments = `SELECT * FROM comments`;
+
+  [
+    { "condition": checkIfEmptyPosts, "action": insertInitialPosts },
+    { "condition": checkIfEmptyComments, "action": insertInitialComments }
+  ].forEach(querySet => checkBeforeExecuteSQLquery(querySet));
+
 });
 
+function checkBeforeExecuteSQLquery(querySet) {
+  if (typeof querySet !== "object") {
+    console.log("Wrong input querySet!");
+    return;
+  }
+  mysqldb.query(querySet.condition, function (err, results, fields) {
+    if (err) {
+      console.log(query.condition + " => " + err.message);
+    }
+    if (results.length === 0) {
+      executeSQLquery(querySet.action);
+    }
+  });
+}
+
+
+function executeSQLquery(query) {
+  mysqldb.query(query, function (err, results, fields) {
+    if (err) {
+      console.log(query + " => " + err.message);
+    }
+  });
+}
+
+
 app.get('/', (req, res) => {
-  mysqldb.query("SELECT * FROM users;", (err, data) => {
-      (err) ? res.send(err) : res.json({users: data});
+  mysqldb.query("SELECT * FROM posts;", (err, data) => {
+    (err) ? res.send(err) : res.json({ users: data });
   });
 });
 
